35b43b3f5039490b81f376a9f13f83786d77c3e3
chika0801 <88967758+chika0801@users.noreply.github.com>
2021-09-17 03:41:09 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+#安装Nginx
+
 apt install -y gnupg2
 
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor > /usr/share/keyrings/nginx-archive-keyring.gpg
@@ -16,23 +18,37 @@ mkdir -p /etc/systemd/system/nginx.service.d
 
 printf "[Service]\nExecStartPost=/bin/sleep 0.1\n" > /etc/systemd/system/nginx.service.d/override.conf
 
+#安装Xray
+
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install
+
+#设置SSL证书权限
 
 chown -R nobody:nogroup /etc/ssl/private/
 
+#下载Nginx配置文件
+
 wget -qO /etc/nginx/nginx.conf https://raw.githubusercontent.com/chika0801/tool/main/nginx.conf
 
+#下载Xray配置文件
+
 wget -qO /usr/local/etc/xray/config.json https://raw.githubusercontent.com/chika0801/tool/main/config.json
+
+#下载geodata文件
 
 wget -qO /usr/local/share/xray/geosite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
 
 wget -qO /usr/local/share/xray/geoip.dat https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
+
+#重启Nginx和Xray
 
 systemctl daemon-reload
 
 systemctl restart nginx
 
 systemctl restart xray
+
+#设置自动更新geodata
 
 printf "* 7 * * * /root/update_geodata.sh\n" > /root/update_geodata
 
