a9374ffd0868e249829f61ff6940c435a5d9b2dd
chika0801 <88967758+chika0801@users.noreply.github.com>
2021-12-01 21:32:14 +0800

    Update speedtest_arm.sh

--- speedtest_arm.sh
+++ speedtest_arm.sh
@@ -17,24 +17,12 @@ speed_test() {
 speed() {
     speed_test '44932'  'Cloudflare'
     speed_test '9916'  'Los Angeles'
-    speed_test '35980'  'San Jose'
     speed_test '28910'  'Tokyo'
-    speed_test '6527'  'Seoul'
-    speed_test '11703'  'Taipei'
     speed_test '28912'  'Hong Kong'
-    speed_test '13058'  'Singapore'
+    speed_test '20637'  'Singapore'
     speed_test '3633'  'Shanghai CT'
     speed_test '24447'  'Shanghai CU'
     speed_test '25637'  'Shanghai CM'
-    speed_test '35722'  'Tianjin CT'
-    speed_test '5505'  'Beijing CU'
-    speed_test '25858'  'Beijing CM'
-    speed_test '27794'  'Nanning CT'
-    speed_test '26678'  'Guangzhou CU'
-    speed_test '15863'  'Nanning CM'
-    speed_test '29071'  'Chengdu CT'
-    speed_test '39012'  'Kunming CU'
-    speed_test '17584'  'Chongqing CM'
 }
 
 mkdir -p /root/speedtest-cli && curl -sSLo /root/speedtest-cli/speedtest https://github.com/chika0801/tool/raw/main/speedtest_arm && chmod +x /root/speedtest-cli/speedtest
