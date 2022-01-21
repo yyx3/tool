5beab3c46027c9806fb0291b62f563551c461b2d
chika0801 <88967758+chika0801@users.noreply.github.com>
2022-01-21 21:35:39 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -17,7 +17,7 @@ speed_test() {
 speed() {
     speed_test '44932'  'Cloudflare'
     speed_test '14236'  'Los Angeles'
-    speed_test '28910'  'Tokyo'
+    speed_test '24333'  'Tokyo'
     speed_test '37390'  'Hong Kong'
     speed_test '20637'  'Singapore'
     speed_test '3633'  'Shanghai CT'
