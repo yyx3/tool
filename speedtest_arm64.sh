da439714effa76e1ce8421b3403641abff1d6808
chika0801 <88967758+chika0801@users.noreply.github.com>
2022-08-06 23:14:45 +0800

    Update speedtest_arm64.sh

--- speedtest_arm64.sh
+++ speedtest_arm64.sh
@@ -21,7 +21,6 @@ speed() {
     speed_test '9916'  'Los Angeles'
     speed_test '6030'  'New York'
     speed_test '9913'  'Amsterdam'
-    speed_test '34270'  'Helsinki'
 }
 
 mkdir -p /root/speedtest-cli && curl -sSLo /root/speedtest-cli/speedtest https://github.com/chika0801/tool/raw/main/speedtest_arm64 && chmod +x /root/speedtest-cli/speedtest
