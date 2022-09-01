bc308a5c433fc884aaba30f52e893fc53a4028f2
chika0801 <88967758+chika0801@users.noreply.github.com>
2022-09-01 11:42:12 +0800

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -27,4 +27,4 @@ speed() {
 
 mkdir -p /root/speedtest-cli && curl -sSLo /root/speedtest-cli/speedtest https://github.com/chika0801/tool/raw/main/speedtest && chmod +x /root/speedtest-cli/speedtest
 clear && printf "%-18s%-18s%-20s%-12s\n" " Node Name" "Upload Speed" "Download Speed" "Latency"
-speed && rm -fr speedtest-cli && rm -fr .config
+speed && rm -fr speedtest-cli && rm -fr .config/ookla
